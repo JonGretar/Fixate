03e56d3a5d4e53b48ad92e024a2dc5b60c3bce03
Jón Grétar Borgþórsson <jon@montel.fi>
2026-04-26 21:06:37 +0300

    CI Pipelineing

--- mix.exs
+++ mix.exs
@@ -7,10 +7,16 @@ defmodule Fixate.MixProject do
       version: "0.1.0",
       description: description(),
       package: package(),
-      elixir: "~> 1.6",
+      elixir: "~> 1.14",
       licenses: ["MIT"],
       docs: docs(),
-      deps: [{:ex_doc, "~> 0.20", only: :dev}]
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/#{Mix.env()}.plt"},
+        plt_add_apps: [:ex_unit, :mix]
+      ],
+      preferred_cli_env: [precommit: :test],
+      aliases: aliases()
     ]
   end
 
@@ -27,7 +33,6 @@ defmodule Fixate.MixProject do
   end
 
   defp package do
-    # These are the default files included in the package
     [
       files: ["lib", "mix.exs", "README.md"],
       maintainers: ["Jón Grétar Borgþórsson"],
@@ -43,5 +48,23 @@ defmodule Fixate.MixProject do
     ]
   end
 
+  defp deps do
+    [
+      {:ex_doc, "~> 0.20", only: :dev},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
 
+  defp aliases do
+    [
+      precommit: [
+        "compile --warnings-as-errors",
+        "format --check-formatted",
+        "test",
+        "dialyzer",
+        "credo --strict"
+      ]
+    ]
+  end
 end
